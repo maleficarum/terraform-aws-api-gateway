938e5f19c7f62a5504b5a7dec396d531f8bfc68c
Oscar Ivan <o@maleficarum.mx>
2025-05-19 11:17:39 -0600

    no changes. only a fresh update

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "api_gateway_definition" {
-    description = "API Gateway definition"
-    type = object({
-      api_name = string
-      path_part = string
-    })
+  description = "API Gateway definition"
+  type = object({
+    api_name  = string
+    path_part = string
+  })
 }
 
 variable "environment" {
-    type = string
-    description = "Environment name"
+  type        = string
+  description = "Environment name"
 }
 
 variable "public_dns_name" {
-  type = string
+  type        = string
   description = "DNS Name of public DNS"
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "Region"
 }
